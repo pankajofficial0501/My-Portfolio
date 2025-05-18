d72222fdc5cc899e9bb7a3862a73a38f1d336706
pankajofficial0501 <pankajofficial0501@gmail.com>
2025-05-18 22:41:22 +0530

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
-
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import About from './Components/About/About'
+import Contact from './Components/Contact/Contact'
+import Footer from './Components/Footer/Footer'
+import Skills from './Components/Skills/Skills'
+import Education from './Components/Education/Education'
+import Experience from './Components/Experince/Experience'
+import Work from './Components/Work/Work'
 
 import './App.css'
 
@@ -6,9 +14,20 @@ function App() {
  
 
   return (
-    <>
-     <p className='text-[10px]'>Hello World</p>
-    </>
+    <div className='bg-[#050414]'>
+      <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
+      <div className='relative pt-20'>
+        <Navbar />
+        <About />
+        <Skills />
+        <Experience />
+        <Work />
+        <Education />
+        <Contact />
+        <Footer />
+      </div>
+     
+    </div>
   )
 }
 
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { FiMenu , FiX } from 'react-icons/fi'
+import { FaGithub , FaLinkedinIn } from 'react-icons/fa'
+
+
+const Navbar = () => {
+  return (
+    <nav className='bg-transparent  '>
+        <div className='text-white py-5 flex justify-between items-center'>
+            <span className='text-[#8245ec]'>&lt;</span>
+
+        </div>
+      
+    </nav>
+  )
+}
+
+export default Navbar
